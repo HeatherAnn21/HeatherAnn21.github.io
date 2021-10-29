4e16c1bd9fe04a842a61faaff4032542970f8ec7
HeatherAnn21 <heatheranns21@gmail.com>
2021-10-29 02:34:07 -0700

    .

--- portfolio_HS.js
+++ portfolio_HS.js
@@ -1,4 +1,4 @@
-//Used to open and close the cpntact form 
+//Used to open and close the contact form 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 } 
@@ -27,7 +27,7 @@ function showSlides(n) {
     if (n > slides.length) {slideIndex = 1}; 
     if (n < 1) {slideIndex = slides.length}; 
     for (i = 0; i < slides.length; i++) {
-        slides [i].style.display = "none"; 
+        slides[i].style.display = "none"; 
     } 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
